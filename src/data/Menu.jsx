380c210fa63067243a934bbf389ede4ef3edd749
Beimnet27 <beimnetgenene27@gmail.com>
2025-01-31 23:34:26 -0800

    FEAT Add menu data for quick eats and daily delights

---
+++ src/data/Menu.jsx
@@ -0,0 +1,23 @@
+const quickEats = [
+    { id: 1, name: "Pizza Margherita", price: 100, image: "/PizzaMargherita.jpg" },
+    { id: 2, name: "Classic Burger", price: 180, image: "/ClassicBurger.jpg" },
+    { id: 3, name: "Pasta Carbonara", price: 120, image: "/PastaCarbonara.jpg" },
+    { id: 4, name: "Caesar Salad", price: 170, image: "/CaesarSalad.jpg" },
+    { id: 5, name: "Grilled Sandwich", price: 160, image: "/GrilledSandwich.jpg" },
+    { id: 6, name: "French Fries", price: 240, image: "/FrenchFries.jpg" },
+  ];
+  
+  const dailyDelights = [
+    { id: 7, name: "BBQ Ribs", price: 415, image: "/BBQRibs.jpg" },
+    { id: 8, name: "Chicken Wings", price: 599, image: "/ChickenWings.jpg" },
+    { id: 9, name: "Vegetable Soup", price: 150, image: "/VegetableSoup.jpg" },
+    { id: 10, name: "Steak", price: 520, image: "/Steak.jpg" },
+    { id: 11, name: "Seafood Platter", price: 250, image: "/SeafoodPlatter.jpg" },
+    { id: 12, name: "Veggie Burger", price: 199, image: "/VeggieBurger.jpg" },
+    { id: 13, name: "Fruit Smoothie", price: 250, image: "/FruitSmoothie.jpg" },
+    { id: 14, name: "Tacos", price: 180, image: "/Tacos.jpg" },
+    { id: 15, name: "Cheesecake", price: 460, image: "/Cheesecake.webp" },
+  ];
+  
+  export { quickEats, dailyDelights };
+  
